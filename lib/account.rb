6e51ab64f190dbeedfbac48612c66b1daccdec5f
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-06-28 18:48:59 +0200

    basic functionality for adding money to implemented

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,17 @@
 class Account
 
+  MAX_DEPOSIT = 10000
+
   def initialize
     @balance = 0
   end
 
   def getBalance
     return @balance
+  end
+
+  def deposit(sum)
+    @balance += sum unless sum > MAX_DEPOSIT
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,10 +1,21 @@
 require 'account'
 
 describe Account do
-  let(:account) { Account.new }
+  let(:subject) { Account.new }
+  let(:oneThousand) { 1000 }
 
   it 'initializes with a balance of 0' do
-    expect(account.getBalance).to eq 0
+    expect(subject.getBalance).to eq 0
+  end
+
+  it 'allows the user to deposit money' do
+    subject.deposit(oneThousand)
+    expect(subject.getBalance).to eq oneThousand
+  end
+
+  it 'does not allow the user to deposit more than 10,000 at once' do
+    subject.deposit(Account::MAX_DEPOSIT)
+    expect(subject.getBalance).to eq oneThousand
   end
 
 end
