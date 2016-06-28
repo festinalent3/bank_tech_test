c77ccf6037a71797597edae30182f486f4734d7c
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-06-28 18:56:41 +0200

    quick refactoring of tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,29 +8,36 @@ describe Account do
     expect(subject.getBalance).to eq 0
   end
 
-  it 'allows the user to deposit money' do
-    subject.deposit(oneThousand)
-    expect(subject.getBalance).to eq oneThousand
+  describe '#deposit' do
+
+    it 'increases the balance of the account' do
+      subject.deposit(oneThousand)
+      expect(subject.getBalance).to eq oneThousand
+    end
+
+    it 'does not allow the user to deposit more than 10,000 at once' do
+      subject.deposit(Account::MAX_DEPOSIT + 1)
+      expect(subject.getBalance).to eq 0
+    end
+
   end
 
-  it 'does not allow the user to deposit more than 10,000 at once' do
-    subject.deposit(Account::MAX_DEPOSIT + 1)
-    expect(subject.getBalance).to eq 0
-  end
+  describe '#withdraw' do
 
-  it 'allows the user to whitdraw money' do
-    subject.deposit(oneThousand)
-    subject.withdraw(oneThousand)
-    expect(subject.getBalance).to eq 0
-  end
+    it 'decreases the balance of the account' do
+      subject.deposit(oneThousand)
+      subject.withdraw(oneThousand)
+      expect(subject.getBalance).to eq 0
+    end
 
-  it 'does not allow the user to withdraw money if balance = 0 or if the balance would go below zero' do
-    subject.withdraw(oneThousand)
-    expect(subject.getBalance).to eq 0
-  end
+    it 'does not allow the user to withdraw money if balance = 0' do
+      subject.withdraw(oneThousand)
+      expect(subject.getBalance).to eq 0
+    end
 
-  it 'does not allow the user to withdraw money if the balance would go below zero' do
-    subject.withdraw(oneThousand + 1)
-    expect(subject.getBalance).to eq 0
+    it 'does not allow the user to withdraw money if the balance would go below zero' do
+      subject.withdraw(oneThousand + 1)
+      expect(subject.getBalance).to eq 0
+    end
   end
 end
